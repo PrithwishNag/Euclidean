22c52d64face5ab0e669a723314a7274df700bf2
PrithwishNag <prithwish.nag@gmail.com>
2021-10-06 20:31:07 +0530

    Queue done!

--- music.py
+++ music.py
@@ -2,13 +2,15 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import time
+from collections import deque
+import asyncio
 
 
 class music(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.vc = None
-        self.is_paused = 0
+        self.is_paused = False
         self.OPTIONS = {
             "FFMPEG": {
                 "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
@@ -17,6 +19,7 @@ class music(commands.Cog):
             "YDL": {"format": "worstaudio", "noplaylist": "True"},
         }
         self.ydl = youtube_dl.YoutubeDL(self.OPTIONS["YDL"])
+        self.song_queue = deque()
 
     @commands.command()
     async def connect(self, ctx):
@@ -28,6 +31,7 @@ class music(commands.Cog):
             await vc.connect()
         else:
             await ctx.voice_client.move_to(vc)
+        self.vc = ctx.voice_client
         return 1
 
     @commands.command()
@@ -49,7 +53,20 @@ class music(commands.Cog):
     #             await ctx.send(f"{i}. \"{info['track']}\", \"{info['artist']}\"")
     #     pass
 
-    # def play_next():
+    async def play_next(self, ctx):
+        if self.song_queue == []:
+            return
+
+        song = self.song_queue[0]
+        self.song_queue.popleft()
+
+        await ctx.send(f"Playing \"{song['title']}\" - {song['channel']}")
+        self.vc.play(
+            song["source"],
+            after=lambda err: asyncio.run_coroutine_threadsafe(
+                self.play_next(ctx), self.client.loop
+            ),
+        )
 
     @commands.command()
     async def play(self, ctx, *args):
@@ -57,43 +74,60 @@ class music(commands.Cog):
             return
 
         search = " ".join(args)
-
-        # ctx.voice_client.stop()
-
-        self.vc = ctx.voice_client
-
         info = self.ydl.extract_info(f"ytsearch:{search}", download=False)["entries"][0]
-        print("INFO: ", info)
         url = info["formats"][0]["url"]
         source = await discord.FFmpegOpusAudio.from_probe(
             executable="C:/ffmpeg/bin/ffmpeg.exe",
             source=url,
             **self.OPTIONS["FFMPEG"],
         )
-        self.vc.play(source, after=lambda x: print("done: ", x))
-        await ctx.send(f"Playing \"{info['title']}\", \"{info['artist']}\"")
+        song = {"source": source, "title": info["title"], "channel": info["channel"]}
+        self.song_queue.append(song)
+
+        if not self.vc.is_playing() and not self.is_paused:
+            await self.play_next(ctx)
+        else:
+            await ctx.send(f"Added to queue \"{song['title']}\" - {song['channel']}")
+
+    @commands.command()
+    async def p(self, ctx, *args):
+        await self.play(ctx, *args)
 
     @commands.command()
     async def pause(self, ctx):
-        if self.vc is not None and self.vc.is_playing:
+        if self.vc is not None and self.vc.is_playing():
             self.vc.pause()
-            self.is_paused = 1
+            self.is_paused = True
             await ctx.send("Paused")
+        else:
+            await ctx.send(
+                "Play something noob!"
+            )  # Queue is empty (Nothing is playing)
 
     @commands.command()
     async def resume(self, ctx):
-        print(self.vc)
-        print(self.vc.is_playing)
         if self.vc is not None and self.is_paused:
             self.vc.resume()
-            self.is_paused = 0
+            self.is_paused = False
             await ctx.send("Resumed")
+        else:
+            await ctx.send("Aready playing, you blind?")
 
     @commands.command()
     async def skip(self, ctx):
-        if self.vc is not None and self.vc.is_playing:
+        if self.vc is not None and (self.vc.is_playing() or self.is_paused):
             self.vc.stop()
+            self.is_paused = False
             await ctx.send("Skipped")
+        else:
+            await ctx.send(
+                "Play something noob!"
+            )  # Queue is empty (Nothing is playing)
+
+    @commands.command()
+    async def empty(self, ctx):
+        self.song_queue = []
+        await ctx.send("Queue emptied")
 
 
 def setup(client):
