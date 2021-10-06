902553fb3d6633a98226d306b660fc872d8a33f6
PrithwishNag <prithwish.nag@gmail.com>
2021-10-06 14:05:15 +0530

    Play done!

--- music.py
+++ music.py
@@ -9,17 +9,26 @@ class music(commands.Cog):
         self.client = client
         self.vc = None
         self.is_paused = 0
+        self.OPTIONS = {
+            "FFMPEG": {
+                "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+                "options": "-b:a 48k -vn",
+            },
+            "YDL": {"format": "worstaudio", "noplaylist": "True"},
+        }
+        self.ydl = youtube_dl.YoutubeDL(self.OPTIONS["YDL"])
 
     @commands.command()
     async def connect(self, ctx):
         if ctx.author.voice is None:
             await ctx.send("Voice channel join kar re hajjam!")
-            return
+            return 0
         vc = ctx.author.voice.channel
         if ctx.voice_client is None:
             await vc.connect()
         else:
             await ctx.voice_client.move_to(vc)
+        return 1
 
     @commands.command()
     async def disconnect(self, ctx):
@@ -28,34 +37,41 @@ class music(commands.Cog):
             return
         await ctx.voice_client.disconnect()
 
-    @commands.command()
-    async def search(self, ctx, *args):
-        pass
+    # @commands.command()
+    # async def search(self, ctx, *args):
+    #     search = " ".join(args)
+    #     with youtube_dl.YoutubeDL(self.OPTIONS["YDL"]) as ydl:
+    #         songs = ydl.extract_info(f"ytsearch:{search}", download=False)["entries"]
+    #         print(len(songs))
+    #         for i, s in enumerate(songs):
+    #             info = s
+    #             print(info)
+    #             await ctx.send(f"{i}. \"{info['track']}\", \"{info['artist']}\"")
+    #     pass
+
+    # def play_next():
 
     @commands.command()
     async def play(self, ctx, *args):
-        if ctx.voice_client is None:
-            await self.connect(ctx)
+        if ctx.voice_client is None and not await self.connect(ctx):
+            return
+
         search = " ".join(args)
 
-        ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {
-            "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-            "options": "-b:a 48k -vn",
-        }
-        YDL_OPTIONS = {"format": "worstaudio", "noplaylist": "True"}
+        # ctx.voice_client.stop()
+
         self.vc = ctx.voice_client
 
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(f"ytsearch:{search}", download=False)["entries"][0]
-            print("INFO: ", info)
-            url = info["formats"][0]["url"]
-            source = await discord.FFmpegOpusAudio.from_probe(
-                executable="E:/DiscordBot/Euclidean/ffmpeg/bin/ffmpeg.exe",
-                source=url,
-                **FFMPEG_OPTIONS,
-            )
-            self.vc.play(source, after=lambda x: print("done: ", x))
+        info = self.ydl.extract_info(f"ytsearch:{search}", download=False)["entries"][0]
+        print("INFO: ", info)
+        url = info["formats"][0]["url"]
+        source = await discord.FFmpegOpusAudio.from_probe(
+            executable="C:/ffmpeg/bin/ffmpeg.exe",
+            source=url,
+            **self.OPTIONS["FFMPEG"],
+        )
+        self.vc.play(source, after=lambda x: print("done: ", x))
+        await ctx.send(f"Playing \"{info['title']}\", \"{info['artist']}\"")
 
     @commands.command()
     async def pause(self, ctx):
